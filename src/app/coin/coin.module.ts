02dc2e7c85cc8442e5326e982e4266de5507394f
nanokozmos <ovuncuzun@gmail.com>
2021-06-03 13:54:51 +0100

    adding angular material

--- src/app/coin/coin.module.ts
+++ src/app/coin/coin.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoinListComponent } from './coin-list/coin-list.component';
 import { CoinDetailComponent } from './coin-detail/coin-detail.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -9,7 +14,11 @@ import { CoinDetailComponent } from './coin-detail/coin-detail.component';
     CoinDetailComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCardModule,
   ]
 })
 export class CoinModule { }
